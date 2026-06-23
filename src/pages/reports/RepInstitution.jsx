13a0d10a02ce9b8dc83f34566768cb56c9a14399
njhpsprtraining-bit <njhps.pr.training@gmail.com>
2026-06-23 06:01:51 +0530

    Add src/pages/reports/RepInstitution.jsx

---
+++ src/pages/reports/RepInstitution.jsx
@@ -0,0 +1,9 @@
+// src/pages/reports/RepInstitution.jsx
+export default function RepInstitution({ navigate }) {
+  return (
+    <div>
+      <div className="page-hdr"><div className="page-title">RepInstitution</div></div>
+      <div className="alert alert-info">Report being built.</div>
+    </div>
+  );
+}
