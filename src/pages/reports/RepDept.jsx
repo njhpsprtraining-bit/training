1683830f615e26e773cbc6e0987003234b702173
njhpsprtraining-bit <njhps.pr.training@gmail.com>
2026-06-23 06:01:50 +0530

    Add src/pages/reports/RepDept.jsx

---
+++ src/pages/reports/RepDept.jsx
@@ -0,0 +1,9 @@
+// src/pages/reports/RepDept.jsx
+export default function RepDept({ navigate }) {
+  return (
+    <div>
+      <div className="page-hdr"><div className="page-title">RepDept</div></div>
+      <div className="alert alert-info">Report being built.</div>
+    </div>
+  );
+}
