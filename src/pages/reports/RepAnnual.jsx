972d64c082270a6e5f33692c3c66396f1a3897cb
njhpsprtraining-bit <njhps.pr.training@gmail.com>
2026-06-23 06:01:53 +0530

    Add src/pages/reports/RepAnnual.jsx

---
+++ src/pages/reports/RepAnnual.jsx
@@ -0,0 +1,9 @@
+// src/pages/reports/RepAnnual.jsx
+export default function RepAnnual({ navigate }) {
+  return (
+    <div>
+      <div className="page-hdr"><div className="page-title">RepAnnual</div></div>
+      <div className="alert alert-info">Report being built.</div>
+    </div>
+  );
+}
