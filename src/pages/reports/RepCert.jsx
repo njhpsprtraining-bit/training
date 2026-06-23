4670e5038942b28d8713fc1f89630a9c1b6ede10
njhpsprtraining-bit <njhps.pr.training@gmail.com>
2026-06-23 06:01:52 +0530

    Add src/pages/reports/RepCert.jsx

---
+++ src/pages/reports/RepCert.jsx
@@ -0,0 +1,9 @@
+// src/pages/reports/RepCert.jsx
+export default function RepCert({ navigate }) {
+  return (
+    <div>
+      <div className="page-hdr"><div className="page-title">RepCert</div></div>
+      <div className="alert alert-info">Report being built.</div>
+    </div>
+  );
+}
