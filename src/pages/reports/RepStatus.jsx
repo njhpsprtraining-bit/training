8fa62b3d0649d74e16c4e873df382faadfc88e70
njhpsprtraining-bit <njhps.pr.training@gmail.com>
2026-06-23 06:01:49 +0530

    Add src/pages/reports/RepStatus.jsx

---
+++ src/pages/reports/RepStatus.jsx
@@ -0,0 +1,9 @@
+// src/pages/reports/RepStatus.jsx
+export default function RepStatus({ navigate }) {
+  return (
+    <div>
+      <div className="page-hdr"><div className="page-title">RepStatus</div></div>
+      <div className="alert alert-info">Report being built.</div>
+    </div>
+  );
+}
